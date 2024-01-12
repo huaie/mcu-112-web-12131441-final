ca17839ccc33896431553d6e39419a6a37c40029
huaie <bryanwen29>
2024-01-12 22:30:16 +0800

    1/12

--- src/app/tag-list/tag-list.component.ts
+++ src/app/tag-list/tag-list.component.ts
@@ -1,14 +1,17 @@
 import { Component, HostBinding } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { TagComponent } from '../tag/tag.component';
 
 @Component({
   selector: 'app-tag-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [TagComponent],
   templateUrl: './tag-list.component.html',
   styleUrl: './tag-list.component.css'
 })
 export class TagListComponent {
   @HostBinding('class') class ='tag-list';
+
+  tags = [
+    'enim', 'repellat', 'est', 'eos'];
 
 }
