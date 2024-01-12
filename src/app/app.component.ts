d9400a32471436b61cc4e8c1df76a9c4750e5f4d
huaie <bryanwen29>
2024-01-12 16:17:45 +0800

    feat(footer):頁尾元件實作新增

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router'
 import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavBarComponent, FooterComponent]
 })
 export class AppComponent {
 title = 'mcu-112-web-12131441-final'
