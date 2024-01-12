f16118ea3bf8d502308abed3c7eb6af3db8283a4
huaie <bryanwen29>
2024-01-12 21:34:07 +0800

    feat(tag-list):標籤清單元件實作

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TagListComponent } from '../tag-list/tag-list.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [],
+  imports: [TagListComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
 })
