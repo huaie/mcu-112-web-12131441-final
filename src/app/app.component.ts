fe3d6dba6557353557f22535fe774f3e45b81296
huaie <bryanwen29>
2024-01-12 14:39:10 +0800

    feat(nav):建立導覽元件實作

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-
 import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+title = 'mcu-112-web-12131441-final'
 }
